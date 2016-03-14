ebc723dbeb4f6e17f7685b9c98c8b060ed14a5e8
zhangtian <zhangtian@lierda.com>
2016-03-14 16:46:11 +0800

    update videofragment

--- src/com/tt/main/VideoFragment.java
+++ src/com/tt/main/VideoFragment.java
@@ -1,6 +1,11 @@
 package com.tt.main;
 
 import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import android.app.Fragment;
 import android.media.AudioManager;
@@ -10,6 +15,7 @@ import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.style.LineHeightSpan.WithDensity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
@@ -29,8 +35,13 @@ public class VideoFragment extends Fragment
 	private SurfaceHolder surfaceHolder;
 	private int currentPosition = 0;
 	private boolean isPlaying;
-	private MediaPlayer mediaPlayer;
-	String fileName = "ad.mp4";
+	private MediaPlayer mediaPlayer=null;
+//	String fileName = "ad.mp4";
+	Timer myTimer=null;
+	int which_file=0;
+	
+	 List<String> allVideoList = new ArrayList<String>();// 视频信息集合
+	
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -40,7 +51,9 @@ public class VideoFragment extends Fragment
         surfaceHolder = sv.getHolder();
      // 为SurfaceHolder添加回调
         surfaceHolder.addCallback(callback);
-        
+        new FindThread().start();
+        myTimer=new Timer();
+        myTimer.schedule(new MyTimerTask(), 5000, 60*1000);
      	//	play(0);
         return rootView;
     }
@@ -60,11 +73,9 @@ public class VideoFragment extends Fragment
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
 			Log.e(TAG, "SurfaceHolder 被创建");
-			//if (currentPosition > 0) {
-				// 创建SurfaceHolder的时候，如果存在上次播放的位置，则按照上次播放位置进行播放
-				play(currentPosition);
-			//	currentPosition = 0;
-			//}
+
+				play(which_file);
+
 		}
 
 		@Override
@@ -94,10 +105,19 @@ public class VideoFragment extends Fragment
 	 * 
 	 * @param msec 播放初始位置    
 	 */
-	protected void play(final int msec) {
+	protected void play(int which) {
 		// 获取视频文件地址
 
-		String path=Environment.getExternalStorageDirectory()+"/"+fileName;
+		String path;
+		if(allVideoList.isEmpty())
+			return;
+		if(allVideoList.get(which)!=null)
+			path=allVideoList.get(which);
+		else{
+			which_file=0;
+			path=allVideoList.get(which_file);
+			}
+		//String path=Environment.getExternalStorageDirectory()+"/"+fileName;
 		Log.e(TAG, "path="+path);
 		
 		
@@ -108,19 +128,16 @@ public class VideoFragment extends Fragment
 			return;
 		}
 		try {
-			Log.e(TAG, "new MediaPlayer()");
-			mediaPlayer = new MediaPlayer();
-			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-			// 设置播放的视频源
-			Log.e(TAG, "setDataSource");
-	//		mediaPlayer.setDataSource(path);
-		//  mediaPlayer.setDataSource(songArrayList.get(songIndex));
+			if(mediaPlayer==null){
+				mediaPlayer = new MediaPlayer();
+				mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+				Log.e(TAG, "setDisplay");
+				mediaPlayer.setDisplay(surfaceHolder);
+			}
 			mediaPlayer.setDataSource(file.getAbsolutePath());
-			// 设置显示视频的SurfaceHolder
-			Log.e(TAG, "setDisplay");
-			mediaPlayer.setDisplay(surfaceHolder);
 			Log.e(TAG, "开始装载");
 			mediaPlayer.prepareAsync();
+			mediaPlayer.setLooping(false);//不循环
 			mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
 
 				@Override
@@ -128,7 +145,7 @@ public class VideoFragment extends Fragment
 					Log.e(TAG, "装载完成");
 					mediaPlayer.start();
 					// 按照初始位置播放
-					mediaPlayer.seekTo(msec);
+					mediaPlayer.seekTo(0);
 					// 设置进度条的最大进度为视频流的最大播放时长
 					//seekBar.setMax(mediaPlayer.getDuration());
 					// 开始线程，更新进度条的刻度
@@ -140,6 +157,7 @@ public class VideoFragment extends Fragment
 				public void onCompletion(MediaPlayer mp) {
 					// 在播放完毕被回调
 					//btn_play.setEnabled(true);
+					 play(which_file++);
 				}
 			});
 
@@ -159,5 +177,105 @@ public class VideoFragment extends Fragment
 		}
 
 	}
-}
+	
+	
+	
+	
+	
+	
+	
+	
+	  
+//	allVideoList = new ArrayList<String>();  
+//	getVideoFile(allVideoList,Environment.getExternalStorageDirectory());// 获得视频文件  
+	  
+	private void getVideoFile(final List<String> list, File file) {// 获得视频文件  
+	  
+	        file.listFiles(new FileFilter() {  
+	  
+	            @Override  
+	            public boolean accept(File file) {  
+	                // sdCard找到视频名称  
+	                String name = file.getName();  
+	  
+	                int i = name.indexOf('.');  
+	                if (i != -1) {  
+	                    name = name.substring(i);  
+	                    if (name.equalsIgnoreCase(".mp4")  
+	                            || name.equalsIgnoreCase(".3gp")  
+	                           // || name.equalsIgnoreCase(".wmv")  
+	                          //  || name.equalsIgnoreCase(".ts")  
+	                          //  || name.equalsIgnoreCase(".rmvb")  
+	                            || name.equalsIgnoreCase(".mov")  
+	                            || name.equalsIgnoreCase(".m4v")  
+	                            || name.equalsIgnoreCase(".avi")  
+	                           // || name.equalsIgnoreCase(".m3u8")  
+	                          //  || name.equalsIgnoreCase(".3gpp")  
+	                          //  || name.equalsIgnoreCase(".3gpp2")  
+	                            || name.equalsIgnoreCase(".mkv")  
+	                           // || name.equalsIgnoreCase(".flv")  
+	                            || name.equalsIgnoreCase(".divx")  
+	                            || name.equalsIgnoreCase(".f4v")  
+	                          //  || name.equalsIgnoreCase(".rm")  
+	                           // || name.equalsIgnoreCase(".asf")  
+	                           // || name.equalsIgnoreCase(".ram")  
+	                            || name.equalsIgnoreCase(".mpg")  
+	                           // || name.equalsIgnoreCase(".v8")  
+	                          //  || name.equalsIgnoreCase(".swf")  
+	                         //   || name.equalsIgnoreCase(".m2v")  
+	                          //  || name.equalsIgnoreCase(".asx")  
+	                          //  || name.equalsIgnoreCase(".ra")  
+	                          //  || name.equalsIgnoreCase(".ndivx")  
+	                            || name.equalsIgnoreCase(".xvid")) {  
+	                       // String vi = new String();  
+//	                        vi.setDisplayName(file.getName());  
+//	                        vi.setPath(file.getAbsolutePath()); 
+	                    	String vi=file.getAbsolutePath();
+	                        list.add(vi);
+	       
+	                        return true;  
+	                    }  
+	                } 
+//	                else if (file.isDirectory()) {  //嵌套还是不嵌套？
+//	                    getVideoFile(list, file);  
+//	                }  
+	                return false;  
+	            }  
+	        });  
+	    }  
+	
+	boolean isPlaying(){
+		if(mediaPlayer==null){
+			return false;
+		}
+		else if(mediaPlayer.isPlaying()){
+			return true;
+		}
+		return false;
+	}
+	
+	
+	class FindThread extends Thread{
+		@Override
+		public void run() {
+			getVideoFile(allVideoList,Environment.getExternalStorageDirectory());// 获得视频文件  
+			super.run();
+		}
 
+	}
+
+	
+	class MyTimerTask extends TimerTask{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			if(!isPlaying()){
+				play(which_file);
+			}
+		}
+		
+	}
+	
+
+}
